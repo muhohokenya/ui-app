15a8dd1287cc238bd190bbd1a183d8987b62d070
muhohoweb <muhohokenya@users.noreply.github.com>
2024-03-03 04:07:43 +0300

    Test nginx proxy

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import axios from "axios";
 const el = ref()
 
 onMounted(() => {
-   axios.get("http://54.80.190.139/api/",{
+   axios.get("http://54.80.190.139/api",{
     headers:{
       "Accept":"application/json",
       "Content-Type":"application/json",
